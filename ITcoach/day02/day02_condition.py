7ab8c31c4cb53b3df51c5350ef361a3b80bf7710
陈旭升 <82516351@qq.com>
2020-05-25 18:07:16 +0800

    20200525

--- ITcoach/day02/day02_condition.py
+++ ITcoach/day02/day02_condition.py
@@ -8,21 +8,21 @@ str = 'fdsafdsaYYYYdsafd43243243!@^%$#复读机啊发动机范德萨就开了发
 """
 str01 = 'fdsafdsaYYYYdsafd43243243!@^%$#复读机啊发动机范德萨就开了发的撒娇'
 
+lower_num = 0
+upper_num = 0
+digit_num = 0
+chinese_num = 0
+other_num = 0
 for s in str01:
-    lower_num = 0
-    upper_num = 0
-    digit_num = 0
-    chinese_num = 0
-    other_num = 0
-    if s.islower:
-        lower_num = lower_num + 1
+    if s.islower():
+        lower_num += 1
     elif s.isupper():
-        upper_num = upper_num + 1
+        upper_num += 1
     elif s.isdigit():
-        digit_num = digit_num + 1
+        digit_num += 1
     elif s >= '\u4e00' and s <= '\u9fa5':
-        chinese_num = chinese_num + 1
+        chinese_num += 1
     else:
-        other_num = other_num + 1
+        other_num += 1
 
-    print('在字符串中：小写字母{}个，大写字母{}个，数字{}个，其它字符{}个。'.format(lower_num,upper_num,digit_num,chinese_num,other_num))
+print('在字符串中：小写字母{}个，大写字母{}个，数字{}个，其它字符{}个。'.format(lower_num,upper_num,digit_num,chinese_num,other_num))
